3fd5f53d7967a9640cde60735038860d834d334c
Ivan Pianetti <ivanpianetti23@gmail.com>
2020-07-10 20:47:00 -0300

    update prettierrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ivan Pianetti`,
+    author: `Ivan Pianetti`,
+    description: `Personal website and blog`,
+    siteUrl: 'https://www.ivanpianetti.dev',
+    social: {
+      twitter: `bipiane`,
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,4 +35,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
